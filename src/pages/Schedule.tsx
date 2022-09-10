24a2ebb9d74b75a71cc1d05ccfd5924a62f788c3
kirillsaint <kirillsaint@wtfuck.tk>
2022-09-10 14:31:22 +0600

    loading

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -11,8 +11,10 @@ import {
 	Container,
 	Button,
 	ButtonGroup,
+	Skeleton,
+	SkeletonText,
+	Spinner,
 } from "@chakra-ui/react";
-import Loader from "../components/Loader";
 import moment from "moment";
 import "moment/locale/ru";
 import Linkify from "react-linkify";
@@ -87,7 +89,15 @@ function Schedule() {
 		marks: { mood: string; value: string }[];
 	}
 
-	const Day = ({ day, dayName }: { day: IDay | null; dayName: string }) => {
+	const Day = ({
+		day,
+		dayName,
+		isLoading,
+	}: {
+		day: IDay | null;
+		dayName: string;
+		isLoading?: boolean;
+	}) => {
 		return (
 			<Stack direction="column" spacing="5px">
 				<Text>{dayName}</Text>
@@ -179,27 +189,127 @@ function Schedule() {
 							})}
 						</Stack>
 					)) || (
-						<Center w="100%" mt="10px" mb="10px">
-							<Text color="rgba(187, 187, 187, 1)" fontWeight="bold">
-								Уроков не найдено
-							</Text>
-						</Center>
+						<>
+							{(!isLoading && (
+								<Center w="100%" mt="10px" mb="10px">
+									<Text color="rgba(187, 187, 187, 1)" fontWeight="bold">
+										Уроков не найдено
+									</Text>
+								</Center>
+							)) || (
+								<Stack direction="column" spacing="0px">
+									{[1, 2, 3, 4].map((key) => (
+										<Stack direction="column" spacing="1px">
+											<Stack
+												direction={["column", "row"]}
+												spacing={["10px", "0px"]}
+												m="20px"
+												mt="10px"
+												mb="10px"
+												justifyContent="space-between"
+											>
+												<Stack
+													direction={["column", "row"]}
+													spacing={["10px", "50px"]}
+												>
+													<Stack direction="column" spacing="1px">
+														<Skeleton>
+															<Text fontSize={18}>Зачем ты это читаешь?</Text>
+														</Skeleton>
+
+														<Skeleton>
+															<Text fontSize={14} color="#AAAAAA">
+																1 урок
+															</Text>
+														</Skeleton>
+													</Stack>
+
+													<Stack direction="row" spacing="2px">
+														<Center>
+															<Skeleton
+																minW="35px"
+																minH="35px"
+																borderRadius="6px"
+															>
+																<Box
+																	alignItems="center"
+																	borderRadius="6px"
+																	color="white"
+																	display="flex"
+																	fontSize="16px"
+																	justifyContent="center"
+																	userSelect="none"
+																	minW="35px"
+																	minH="35px"
+																	overflow="hidden"
+																	bgColor={"rgba(0, 255, 25, 0.5)"}
+																>
+																	<Heading fontSize={20}>5</Heading>
+																</Box>
+															</Skeleton>
+														</Center>
+
+														<Center>
+															<Skeleton
+																minW="35px"
+																minH="35px"
+																borderRadius="6px"
+															>
+																<Box
+																	alignItems="center"
+																	borderRadius="6px"
+																	color="white"
+																	display="flex"
+																	fontSize="16px"
+																	justifyContent="center"
+																	userSelect="none"
+																	minW="35px"
+																	minH="35px"
+																	overflow="hidden"
+																	bgColor={"rgba(0, 255, 25, 0.5)"}
+																>
+																	<Heading fontSize={20}>5</Heading>
+																</Box>
+															</Skeleton>
+														</Center>
+													</Stack>
+												</Stack>
+												<SkeletonText noOfLines={4} spacing="1">
+													<Text>НЕ ЧИТАЙ ЭТО!!</Text>
+												</SkeletonText>
+											</Stack>
+
+											{key !== 4 && (
+												<Divider
+													opacity="1"
+													borderColor="rgba(187, 187, 187, 1)"
+												/>
+											)}
+										</Stack>
+									))}
+								</Stack>
+							)}
+						</>
 					)}
 				</Box>
 			</Stack>
 		);
 	};
 
-	return schedule.length === 0 ? (
-		<Loader />
-	) : (
+	return (
 		<Container maxW={["full", "7xl"]}>
 			<Stack direction="column" spacing="10px">
 				<Stack direction="column" spacing="5px">
-					<Heading size="md">
-						{moment.unix(currentWeek[0]).format("LL")} –{" "}
-						{moment.unix(currentWeek[6]).format("LL")}
-					</Heading>
+					{(currentWeek.length !== 0 && (
+						<Heading size="md">
+							{moment.unix(currentWeek[0]).format("LL")} –{" "}
+							{moment.unix(currentWeek[6]).format("LL")}
+						</Heading>
+					)) || (
+						<SkeletonText noOfLines={1} fontSize={"md"} size="md">
+							<Text>не читай это</Text>
+						</SkeletonText>
+					)}
 					<ButtonGroup variant="outline" spacing="2px">
 						<Button
 							borderRadius="10px"
@@ -231,20 +341,29 @@ function Schedule() {
 						</Button>
 					</ButtonGroup>
 				</Stack>
-				<SimpleGrid columns={[1, 2]} spacing={["10px", "20px"]}>
-					<Stack direction="column" spacing={["10px", "20px"]}>
-						<Day day={schedule[0]} dayName="Понедельник" />
-						<Day day={schedule[1]} dayName="Вторник" />
-						<Day day={schedule[2]} dayName="Среда" />
-					</Stack>
 
-					<Stack direction="column" spacing={["10px", "20px"]}>
-						<Day day={schedule[3]} dayName="Четверг" />
-						<Day day={schedule[4]} dayName="Пятница" />
-						<Day day={schedule[5]} dayName="Суббота" />
-						<Day day={schedule[6]} dayName="Воскресенье" />
-					</Stack>
-				</SimpleGrid>
+				{(schedule.length !== 0 && (
+					<SimpleGrid columns={[1, 2]} spacing={["10px", "20px"]}>
+						<Stack direction="column" spacing={["10px", "20px"]}>
+							<Day day={schedule[0]} dayName="Понедельник" />
+							<Day day={schedule[1]} dayName="Вторник" />
+							<Day day={schedule[2]} dayName="Среда" />
+						</Stack>
+
+						<Stack direction="column" spacing={["10px", "20px"]}>
+							<Day day={schedule[3]} dayName="Четверг" />
+							<Day day={schedule[4]} dayName="Пятница" />
+							<Day day={schedule[5]} dayName="Суббота" />
+							<Day day={schedule[6]} dayName="Воскресенье" />
+						</Stack>
+					</SimpleGrid>
+				)) || (
+					<Box h="100%" w="100%">
+						<Center>
+							<Spinner color="black" size="xl" />
+						</Center>
+					</Box>
+				)}
 			</Stack>
 		</Container>
 	);
